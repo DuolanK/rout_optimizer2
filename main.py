17b711cab3eea94d91203dca449d01fd24982292
DuolanK <Duolan.Kharitonov@mail.ru>
2024-02-24 21:48:10 +0900

    Initial

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def calculate_distance(point1, point2):
 def find_courier_order_distances(orders, couriers):
     courier_order_distances = []
 
-    # Цикл по каждому курьеру
+    # Итерируемся по каждому курьеру
     for courier_id, courier_coordinates in couriers.items():
         distances = []
 
-        # Цикл по каждому заказу
+        # Итерируемся по каждому заказу
         for order_id, order_coordinates in orders.items():
             distance = calculate_distance(courier_coordinates, order_coordinates)
             distances.append((order_id, distance))
@@ -24,7 +24,7 @@ def find_courier_order_distances(orders, couriers):
 
         assigned_orders = []
 
-        # Выбираем один заказ с минимальным расстоянием для каждого курьера
+        # Выбираем только один заказ с минимальным расстоянием для каждого курьера
         for order_id, distance in distances:
             assigned_orders.append((order_id, distance))
             if len(assigned_orders) == 1:
@@ -33,17 +33,40 @@ def find_courier_order_distances(orders, couriers):
 
         courier_order_distances.append((courier_id, assigned_orders))
 
+    # Проверяем, остались ли заказы после первого назначения
+    if orders:
+        # Если заказов больше, чем курьеров, то проводим еще один цикл среди курьеров
+        for courier_id, courier_coordinates in couriers.items():
+            distances = []
+
+            for order_id, order_coordinates in orders.items():
+                distance = calculate_distance(courier_coordinates, order_coordinates)
+                distances.append((order_id, distance))
+
+            distances.sort(key=lambda x: x[1])
+
+            # Выбираем только один заказ для курьера, если есть доступные заказы
+            if distances:
+                order_id, distance = distances[0]
+                courier_order_distances.append((courier_id, [(order_id, distance)]))
+                del orders[order_id]  # Убираем уже назначенный заказ
+
+    # Если курьеров больше, чем заказов, выводим дебаг
+    if len(couriers) > len(courier_order_distances):
+        print("DEBUG: More couriers than orders!")
+
     # Сортируем курьеров по минимальному пути к первому заказу
     courier_order_distances.sort(key=lambda x: x[1][0][1])
 
     return courier_order_distances
 
-# Листы:
+# Пример использования:
 orders = {
     'Order1': (55.7558, 37.6176),
     'Order2': (55.7412, 37.6156),
     'Order3': (55.7522, 37.6221),
     'Order4': (55.7450, 37.6180),
+
 }
 
 couriers = {
@@ -53,10 +76,10 @@ couriers = {
     'Curier4': (56.7350, 37.6150),
 }
 
-
+# Получаем результат
 courier_order_distances = find_courier_order_distances(orders, couriers)
 
-# Выводим результат
+# Выводим результат с пометками о курьерах
 for courier_id, assigned_orders in courier_order_distances:
     print(f"Courier {courier_id}:")
     for order_id, distance in assigned_orders:
